ecb1bcf2b669e4bed3b0355661ac419ff3a87bc0
Manoj Gautam <manojit.gautam@gmail.com>
2019-06-30 16:53:29 +0545

    Updated

--- olib/urls.py
+++ olib/urls.py
@@ -19,7 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.homepage, name="homepage"),
